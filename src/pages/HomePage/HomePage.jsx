f3ed66f6747b2658ca86d08fac0be041151daa8c
TheCodingFreak-Github <thecodingfreak.contact@gmail.com>
2021-12-03 15:14:09 +0100

    react-boilerplate v1.0

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './HomePage.scss';
+
+const HomePage = (props) => {
+  return ( <>
+    <h1>HomePage component</h1>
+  </> );
+}
+ 
+export default HomePage;
---
+++ templates/component/component/TemplateName.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TemplateName.scss';
+
+const TemplateName = (props) => {
+  return ( <>
+    <h1>TemplateName component</h1>
+  </> );
+}
+ 
+export default TemplateName;
